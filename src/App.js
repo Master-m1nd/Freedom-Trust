03e7f30f409d7407c851e1e4eff1eff14742ae90
Master-m1nd <lukachynetsdean@gmail.com>
2023-08-22 15:12:13 +0300

    components LogIn & SignUp are done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './conts/Sign-Up/SignUp';
+import LogIn from './conts/Log-in/LogIn';
 
 function App() {
     return (
-        <div className='App'>
-        </div>
+        <BrowserRouter>
+            <div className='App'> 
+                <Routes>
+                    <Route path = '/signUp' element = {<SignUp/>}/>
+                    <Route path = '/logIn' element = {<LogIn/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
